117b46d6b1a08dd4262f42ecf1b75e5fd7b2975d
yudhiazhr <m.yudhiazhari@gmail.com>
2024-02-20 12:59:42 +0700

    feat: tambah fitur payment gateway midtrans

--- payment-page.php
+++ payment-page.php
@@ -1,10 +1,63 @@
 <?php 
-
+namespace Midtrans;
 session_start();
 
 if (isset($_POST['order_pay_btn'])) {
    $order_status =  $_POST['order_status'];
    $order_total_price = $_POST['order_total_price'];
+}
+
+// Set your server key
+$server_key = 'SB-Mid-server-10Dr4ULfMa42pHA6VbJOxEOt';
+
+if(isset($_POST['order_status']) && $_POST['order_status'] == "not paid") {
+    // Include the Midtrans library
+    require_once('midtrans/Midtrans.php');
+
+    // Set your client key
+    Config::$clientKey = 'SB-Mid-client-A4xo8S8KfljkK5QP';
+    // Set your server key
+    Config::$serverKey = $server_key;
+    // Set to development mode
+    Config::$isProduction = false;
+
+    // Transaction details
+    $transaction_details = array(
+        'order_id' => 'ORDER-' . rand(),
+        'gross_amount' => $_POST['order_total_price'], // amount in Rupiah
+    );
+
+    // Customer details
+    $customer_details = array(
+        'user_name'    => 'testing',
+        'email'         => 'test@gmail.com',
+    );
+
+    // Transaction data
+    $transaction_data = array(
+        'transaction_details' => $transaction_details,
+        'customer_details'    => $customer_details,
+    );
+
+    $snap_token = '';
+try {
+  $snap_token = Snap::getSnapToken($transaction_data);
+}
+catch (\Exception $e) {
+    echo $e->getMessage();
+}
+
+function printExampleWarningMessage() {
+    if (strpos(Config::$serverKey, 'your ') != false ) {
+        echo "<code>";
+        echo "<h4>Please set your server key from sandbox</h4>";
+        echo "In file: " . __FILE__;
+        echo "<br>";
+        echo "<br>";
+        echo htmlspecialchars('Config::$serverKey = \'SB-Mid-server-10Dr4ULfMa42pHA6VbJOxEOt\';');
+        die();
+    } 
+}
 
 }
 
@@ -28,10 +81,15 @@ if (isset($_POST['order_pay_btn'])) {
     <!-- css -->
     <link rel="stylesheet" href="css/checkout.css">
 
+    <!-- JS -->
+    <script type="text/javascript"
+      src="https://app.sandbox.midtrans.com/snap/snap.js"
+      data-client-key="SB-Mid-client-A4xo8S8KfljkK5QP"></script>
+
 </head>
 <body>
 
-    <?php include('layouts/navbar.php')?>
+    
 
     <!-- payment -->
     <section class="payment my-5 py-5">
@@ -47,8 +105,34 @@ if (isset($_POST['order_pay_btn'])) {
                 
             <?php } else if (isset($_POST['order_status']) && $_POST['order_status'] == "not paid") { ?>
                     <p>Total payment: IDR <?php echo $_POST['order_total_price'];?></p>
-                    <input type="submit" class="btn btn-primary" value="Pay now">
-            
+                    <input id="pay-button" type="submit" class="btn btn-primary" value="Pay now">
+                    
+
+    <script type="text/javascript">
+      // For example trigger on button clicked, or any time you need
+      var payButton = document.getElementById('pay-button');
+      payButton.addEventListener('click', function () {
+        // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token
+        window.snap.pay('<?php echo $snap_token?>', {
+          onSuccess: function(result){
+            /* You may add your own implementation here */
+            alert("payment success!"); console.log(result);
+          },
+          onPending: function(result){
+            /* You may add your own implementation here */
+            alert("wating your payment!"); console.log(result);
+          },
+          onError: function(result){
+            /* You may add your own implementation here */
+            alert("payment failed!"); console.log(result);
+          },
+          onClose: function(){
+            /* You may add your own implementation here */
+            alert('you closed the popup without finishing the payment');
+          }
+        })
+      });
+    </script>
             <?php } else { ?>
               <p>You dont have an order</p>
             <?php } ?>
